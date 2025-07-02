2f709fb5a8ef466715918d2cd9cc59e997eb4b89
CatShredia <sasha990g@gmail.com>
2025-07-02 11:19:22 +0300

    v0.0.2pr stylizing
    v0.0.2pr react components stylizing
    
    v0.0.2pr stylizing

---
+++ 2practise/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import "./assets/style.css";
+
+import Header from './includes/header'
+import Main from './includes/main'
+
+import logo from "./assets/logo.png";
+import arom from "./assets/arom.jpg";
+import shin from "./assets/shin.jpeg";
+import maslo from "./assets/maslo.jpeg";
+function App() {
+  return (
+    <>
+      <div>
+        <Header></Header>
+        <Main></Main>
+      </div>
+    </>
+  );
+}
+export default App;
---
+++ 2practise/src/includes/header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from "../assets/logo.png";
+import Button from '../components/Button';
+
+class header extends React.Component {
+    render() {
+        return (
+            <header className="header container">
+                <div className="header-top">
+                    <div className="header-left">
+                        <Button content="О нас" colorText="blue" href="#" colorBack="none"></Button>
+                        <Button content="Каталог" colorText="black" href="#" colorBack="none"></Button>
+                        <Button content="Где найти нас" colorText="black" href="#" colorBack="none"></Button>
+                    </div>
+                    <img src={logo} alt="Ошибка" className="header-img" />
+                    <div className="header-right">
+                        <Button content="Регистрация" colorText="white" href="#" colorBack="rgb(33, 33, 33)"></Button>
+                        <Button content="Вход" colorText="white" href="#" colorBack="blue"></Button>
+                    </div>
+                </div>
+                <div></div>
+                <div className="header-bottom--m">
+                    <a className="header-bottom">
+                        <Button
+                            content={
+                                <>
+                                    О компании <span style={{ color: 'blue' }}>АвтоТорг</span>
+                                </>
+                            }
+                            colorText="black"
+                            href="#"
+                            colorBack="rgb(185, 185, 185)"
+                        />
+                    </a>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default header;
