172fc5a2b0e3a6c4055e48f6701b773c2a7b9027
CatShredia <sasha990g@gmail.com>
2025-07-05 09:10:53 +0300

    v0.0.6pr search from_ul_list
    v0.0.6pr catalog_fix

--- 2practise/src/includes/main.jsx
+++ 2practise/src/includes/main.jsx
@@ -1,36 +1,10 @@
 import React from 'react';
-import SellItem from '../components/SellItem';
-import Button from '../components/Button';
-
-import maslo from '../assets/maslo.jpeg'
-import shin from '../assets/shin.jpeg'
-
-import products from '../../data/products.json';
 
 class main extends React.Component {
     render() {
         return (
             <main className='main container'>
-                <div className="main-1">
-                    <p className="main-1-title">Каталог товаров</p>
-                    <div className="main-filter">
-                        <Button colorText="White" colorBack="blue" content="Все товары" href="#"></Button>
-                        <Button colorText="blue" colorBack="white" content="Шины/Колеса" href="#"></Button>
-                        <Button colorText="blue" colorBack="white" content="Масла" href="#"></Button>
-                        <Button colorText="blue" colorBack="white" content="Ароматизаторы" href="#"></Button>
-                    </div>
-                    <div className="main-1-cat">
-                        {products.map((product, index) => (
-                            <SellItem
-                                key={index}
-                                img={product["Путь к картинке"]}
-                                title={product["Название"]}
-                                cost={product["Цена"]}
-                                desc={product["Описание"]}
-                            />
-                        ))}
-                    </div>
-                </div>
+
             </main>
         );
     }
--- 2practise/src/pages/Catalog.jsx
+++ 2practise/src/pages/Catalog.jsx
@@ -1,6 +1,9 @@
-import Main from '../includes/main'
+import Button from '../components/Button';
+import SellItem from '../components/SellItem';
+import products from '../../data/products.json';
 
 export default function Home() {
+
     return (
         <>
             <Main></Main>
