7be5da06e3f1d6d3e566fa07f92b8a522b87e134
CatShredia <sasha990g@gmail.com>
2025-07-10 15:01:40 +0300

    v0.0.1 banner component

---
+++ TyganAsh/src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+
+import styles from './Banner.module.css'
+
+const Banner = (props) => {
+    return (
+        <div className={styles.banner}>
+            <img src={props.imgObject} alt="error" className={styles.img} />
+            <div className={styles.content}>
+                <h1 className={styles.slogan}>{props.SloganContent}</h1>
+                <Link to="/catalog" className={styles.button}>{props.buttonContent}</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
--- TyganAsh/src/pages/HomePage.jsx
+++ TyganAsh/src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router";
+import Banner from "../components/Banner";
+
+import bannerImg from '../assets/image.png'
 
 const HomePage = () => {
-    return (<>dd</>);
+    return (
+        <div className="container">
+            <Banner SloganContent={
+                <>
+                    Ближе, чем супермаркет.
+                    <br />
+                    Вкуснее, чем импорт!
+                </>
+            }
+                buttonContent="Я хочу эчпочмак!"
+                imgObject={bannerImg}>
+            </Banner>
+        </div>
+    );
 }
 
 export default HomePage;
