a7886d68c6881be192f71fb2c6ba9702cfb9d09f
CatShredia <sasha990g@gmail.com>
2025-07-10 15:54:12 +0300

    v0.0.2 product json data

--- TyganAsh/src/pages/CatalogPage.jsx
+++ TyganAsh/src/pages/CatalogPage.jsx
@@ -1,15 +1,26 @@
 import Product from "../components/Product";
+import productData from '../../data/products.json';
 
-import imgProduct from '../assets/i.webp'
+import image from '../assets/i.webp'
+
+// Предполагается, что изображения хранятся в ../assets/
+// const images = require.context('../assets', true);
 
 const CatalogPage = () => {
     return (
         <section className="section-products container">
-            <Product img={imgProduct} title="Эчпочмак" description="Треугольные пирожки с мясом, картошкой и луком. Один из символов татарской кухни. "></Product>
-            <Product img={imgProduct} title="Эчпочмак" description="Треугольные пирожки с мясом, картошкой и луком. Один из символов татарской кухни. "></Product>
-            <Product img={imgProduct} title="Эчпочмак" description="Треугольные пирожки с мясом, картошкой и луком. Один из символов татарской кухни. "></Product>
-            <Product img={imgProduct} title="Эчпочмак" description="Треугольные пирожки с мясом, картошкой и луком. Один из символов татарской кухни. "></Product>
-        </section>);
-}
+            {console.log(productData)}
+            {productData.map((product) => (
+                <Product
+                    key={product.id}
+                    img={product.image_url}
+                    // img={images(`./${product.image}`).default}
+                    title={product.title}
+                    description={product.description}
+                />
+            ))}
+        </section>
+    );
+};
 
 export default CatalogPage;
