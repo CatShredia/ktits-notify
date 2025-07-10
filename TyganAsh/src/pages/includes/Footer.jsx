d8bb31d6710194abb22066dcb09bb4e583770609
CatShredia <sasha990g@gmail.com>
2025-07-10 14:42:37 +0300

    v0.0.1 footer include

--- TyganAsh/src/pages/includes/Footer.jsx
+++ TyganAsh/src/pages/includes/Footer.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router";
+
+import LogoImg from '../../assets/logo.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 const Footer = () => {
-    return (<footer className="footer">footer</footer>);
+    return (
+        <footer className="footer container">
+            <Link to="/" className="logo">
+                <img src={LogoImg} alt="error" /></Link>
+            <nav className="footer__links">
+                <dl>
+                    <dt><Link className="footer__link" to="/catalog">Каталог</Link></dt>
+                    <dt><Link className="footer__link" to="/backet">Корзина</Link></dt>
+                    <dt><Link className="footer__link" to="/auth"><FontAwesomeIcon icon={faUser} /></Link></dt>
+                </dl>
+            </nav>
+        </footer>
+    );
 }
 
 export default Footer;
