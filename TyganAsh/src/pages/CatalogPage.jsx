2dc6ddd03fc11534d771afc32bfa614fb7253aa6
CatShredia <sasha990g@gmail.com>
2025-07-11 14:10:09 +0300

    v0.0.4 search products

--- TyganAsh/src/pages/CatalogPage.jsx
+++ TyganAsh/src/pages/CatalogPage.jsx
@@ -1,33 +1,50 @@
+import { useState } from "react";
 import Product from "../components/Product";
 import productData from '../../data/products.json';
 
 const CatalogPage = () => {
+    const [searchTerm, setSearchTerm] = useState("");
 
     const buttons = [
         { type: "link", styles: "product__button_lightnest", path: `/catalog/product`, text: "Посмотреть" },
         { type: "button", styles: "product__button_darknest", path: () => console.log("Подробнее о товаре"), text: "Купить" }
-    ]
+    ];
+
+    const filteredProducts = productData.filter(product =>
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <>
-            <section className="section-search">
-                <input type="text" name="text" id="text" placeholder="text" />
+            <section className="section-search container">
+                <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="Поиск по названию"
+                    className="searchText"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
             </section>
             <section className="section-products container">
-                {productData.map((product) => (
-                    <Product
-                        id={product.id}
-                        key={product.id}
-                        img={product.image_url}
-                        title={product.title}
-                        description={product.description}
-                        cost={product.cost}
-                        buttons={buttons}
-                    />
-                ))}
+                {filteredProducts.length > 0 ? (
+                    filteredProducts.map((product) => (
+                        <Product
+                            id={product.id}
+                            key={product.id}
+                            img={product.image_url}
+                            title={product.title}
+                            description={product.description}
+                            cost={product.cost}
+                            buttons={buttons}
+                        />
+                    ))
+                ) : (
+                    <p>Товары не найдены</p>
+                )}
             </section>
         </>
-
     );
 };
 
